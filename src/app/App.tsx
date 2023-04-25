9fa1110d1f59fe239d95cf52c3fd81313cc5918a
Sergey Sharuha <sergeihar82@gmail.com>
2023-04-25 22:20:28 +0300

    feat: adding dynamic lang form html

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,27 +4,30 @@ import { MainPage } from '../pages'
 
 import './index.css'
 import { Notifications } from '@mantine/notifications'
-import { useEffect } from 'react'
-import { TranslationProvider } from 'src/context/language/translation.context'
+import { useEffect, useRef } from 'react'
+import { useTranslation } from 'src/context/language/translation.context'
 
 export const App = () => {
 	const value = useTheme()
-	return (
+	const { language } = useTranslation()
+	useEffect(() => {
+		document.documentElement.lang = language ?? 'ru'
+	}, [language])
 
-			<TranslationProvider>
-				<MantineProvider
-					withGlobalStyles
-					withNormalizeCSS
-					theme={{ colorScheme: `${value}` }}
-				>
-					<Notifications
-						id="notification"
-						position="top-center"
-						limit={2}
-						zIndex={2077}
-					/>
-					<MainPage />
-				</MantineProvider>
-			</TranslationProvider>
+	return (
+		<MantineProvider
+			withGlobalStyles
+			withNormalizeCSS
+			theme={{ colorScheme: `${value}` }}
+		>
+			<Notifications
+				id="notification"
+				position="top-center"
+				limit={2}
+				zIndex={2077}
+			/>
+			<MainPage />
+		</MantineProvider>
 	)
 }
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import { App } from './app/App'
 import { ThemeProvider } from './context/theme/theme.context'
+import { TranslationProvider } from './context/language/translation.context'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<ThemeProvider>
-		<App />
+		<TranslationProvider>
+			<App />
+		</TranslationProvider>
 	</ThemeProvider>
 )
